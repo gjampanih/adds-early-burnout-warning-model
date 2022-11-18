75b94c647cc61dfc591dca836bb73664099ba210
gjampanih <GirishJampaniHanumantha@iHeartMedia.com>
2022-11-18 09:57:55 -0600

    adds-early-burnout-warning-model: initial-committ

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predictive model to detect burnout among listeners',
+    author='Your name (or your organization/company/team)',
+    license='MIT',
+)
